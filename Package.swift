515d8fac382d75ca3297af4cd714345cb334f3b4
Raymond Pinto <ge73lam@mytum.de>
2020-01-25 00:03:05 +0100

    Updated the Package.swift file

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Prototyper",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "https://github.com/evgenyneu/keychain-swift.git")
+        .package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "18.0.0")
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
