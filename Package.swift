7303e5284a650d3b2f5059a792fb3bcc159319a3
Raymond Pinto <ge73lam@mytum.de>
2020-01-25 00:16:06 +0100

    Added the dependency to the target in Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Prototyper",
-            dependencies: []),
+            dependencies: ["KeychainSwift"]),
         .testTarget(
             name: "PrototyperTests",
             dependencies: ["Prototyper"]),
