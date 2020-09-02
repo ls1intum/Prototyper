fd5ee1814fbc21fa386ab586a6fb2cc4de2651bb
Simon <wwasibo-apple@yahoo.de>
2020-09-02 17:47:31 +0200

    Added Example

---
+++ Example/Prototyper Example/Prototyper_ExampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  Prototyper_ExampleApp.swift
+//  Prototyper Example
+//
+//  Created by Paul Schmiedmayer on 8/24/20.
+//  Copyright © 2020 TUM LS1. All rights reserved.
+//
+
+import SwiftUI
+import Prototyper
+
+
+@main
+struct Prototyper_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
